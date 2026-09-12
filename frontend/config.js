5754b61d1e77faab430e85f6dd0ba401ebda33aa
javimolin-rgb <javimolin243@gmail.com>
2026-09-11 23:35:11 -0300

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // URL pública de tu backend.
 // Para desarrollo: http://localhost:8000
 // Para producción: https://tu-backend.example.com
-window.PODCAST_API_URL = "http://localhost:8000";
+window.PODCAST_API_URL = "https://podcast-api-2z5o.onrender.com";
